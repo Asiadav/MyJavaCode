81eaf76a7339b3c9531b1b3534b25cd814cee410
Asiadav <37916054+Asiadav@users.noreply.github.com>
2019-01-14 12:30:29 -0800

    Quickly updating files

---   compareTo/src/com/company/Main.java
+++   compareTo/src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
+import java.util.Arrays;
 
 public class Main {
 
+    public static void mystery(int[] data, int x, int y){
+        data[data[x]] = data[y];
+        data[y] = x;
+    }
+
+
     public static void main(String[] args) {
-    System.out.println("Sorry".compareTo("shelves"));
+        int[] numbers = {3,7,1,0,25,4,18,-1,5};
+        mystery(numbers, 3,1);
+        mystery(numbers, 5,6);
+        mystery(numbers, 8,4);
+    System.out.println(Arrays.toString(numbers));
         }
 }
